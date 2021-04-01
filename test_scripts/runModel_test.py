1c5a5931928c3ad468fd6852c5b2bf6f9c0cfaa2
arnabmutsuddy <amutsud@g.clemson.edu>
2021-04-01 10:49:44 -0400

    restore point

--- test_scripts/runModel_test.py
+++ test_scripts/runModel_test.py
@@ -20,7 +20,7 @@ sys.path.append(os.getcwd()[0:os.getcwd().rfind('/')]+'/sparced/bin')
 
 from modules.RunSPARCED import RunSPARCED
 
-# %%
+# %%%
 
 
 # SBML model we want to import
@@ -55,7 +55,7 @@ ts = 30
 #
 STIMligs = [100, 100.0, 100.0, 100.0, 100.0, 100.0, 1721.0]  # EGF, Her, HGF, PDGF, FGF, IGF, INS
 # STIMligs = [100.0,0.0,0.0,0.0,0.0,0.0,100.0] # EGF, Her, HGF, PDGF, FGF, IGF, INS
-# STIMligs = [0.0,0.0,0.0,0.0,0.0,0.0,0.0] # EGF, Her, HGF, PDGF, FGF, IGF, INS
+STIMligs = [0.0,0.0,0.0,0.0,0.0,0.0,0.0] # EGF, Her, HGF, PDGF, FGF, IGF, INS
 
 STIMligs_id = ['E', 'H', 'HGF', 'P', 'F', 'I', 'INS']
 
@@ -306,7 +306,7 @@ xoutS_all, xoutG_all, xoutObs_all, tout_all = RunSPARCED(flagD, th, species_init
 #xoutS_final = pd.Series(data=xoutS_all[-1], index=model.getStateIds())
 
 #xoutS_final.to_csv('xoutS_final.csv', sep='\t', index=True)
-# %% test - plot trajectories
+#test - plot trajectories
 # xoutS_all[1960,:]
 
 species_input = pd.read_csv('input_files/Species.txt', sep='\t', header=0, index_col=0)
@@ -325,31 +325,7 @@ for k in range(len(STIMligs)):
 
 mpl.rcParams['figure.dpi'] = 300
 
-#%%#
-species_CC_dash = ['ppERK', 'ppAKT', 'pcFos_cJun', 'cMyc', 'Cd', 'Cdk46', 'Cd_Cdk46', 'Cd_Cdk46_pRB', 'Cd_Cdk46_pRB_E2F', 'pRB', 'pRBp', 'pRBpp', 'pRB_E2F','pRBp_E2F','Ce_Cdk2_p21','Ce_Cdk2_p27', 'E2F', 'Ce', 'Ce_Cdk2', 'Ce_Cdk2_pRBp', 'Ce_Cdk2_pRBp_E2F', 'Cd_Cdk46_p18', 'Cd_Cdk46_p19', 'Cd_Cdk46_p21', 'Cd_Cdk46_p27', 'p18', 'p21', 'p27', 'p57', 'E2Frep']                     
 
-k=0
-
-cc_dash_species, axs_s = plt.subplots(10,3, sharex='col', figsize = (5,7))
-plt.subplots_adjust(hspace = 0.6)
-
-cc_dash_species.suptitle(title,fontsize=5,y=0.92)
-
-for i in range(10):
-    for j in range(3):
-        if k == len(species_CC_dash):
-            break
-        else:
-            y_val = xoutS_all[:, list(species_all).index(species_CC_dash[k])]
-            axs_s[i,j].plot(tout_all/3600, y_val, 'b-')            
-            axs_s[i,j].set_ylim(0,max(y_val)*1.2)
-            axs_s[i,j].tick_params(axis='both', which='major', labelsize='4')
-            axs_s[i,j].ticklabel_format(useOffset=False, style='plain')
-            axs_s[i,j].title.set_text('species: '+species_CC_dash[k]+' (nM)')
-            axs_s[i,j].title.set_size(5)
-            if i == 9:
-                axs_s[i,j].set_xlabel('time(h)', fontsize=5)
-            k +=1
         
 #%%
 species_CC_dash_2 = ['ppERK', 'ppAKT', 'pcFos_cJun', 'cMyc', 'Cd', 'Cdk46',
@@ -510,20 +486,7 @@ obsm = pd.read_csv('input_files/Observables.txt', sep='\t', header=0, index_col=
 
 
 
-#%% CC dashboard
-species_input = pd.read_csv('input_files/Species.txt', sep='\t', header=0, index_col=0)
-
-species_all = species_input.index
-genes_all = pd.read_csv('input_files/GeneReg.txt',sep='\t', header=0, index_col=0).index
-
-obs_all = model.getObservableIds()
-
-
-plt.yticks(fontsize=5)
-
-cc_dash, axs = plt.subplots(4,4, sharex='col')
-
-#%% vTC monitoring
+#%% test module with vTC output
 
 sys.path.append(os.getcwd()[0:os.getcwd().rfind('/')]+'/sparced/bin')
 
@@ -603,6 +566,123 @@ model.setTimepoints(np.linspace(0, ts))  # np.linspace(0, 30) # set timepoints
 
 
 xoutS_all, xoutG_all, xoutObs_all, tout_all, vTC_all, vTCd_all, Nb_all, Nd_all, hills_all, TFa_all, TFr_all = RunSPARCED_test(flagD, th, species_initializations, [], Vn, Vc, model, input_data_folder)
+
+
+
+species_input = pd.read_csv('input_files/Species.txt', sep='\t', header=0, index_col=0)
+
+species_all = species_input.index
+genes_all = pd.read_csv('input_files/GeneReg.txt', sep='\t', header=0, index_col=0).index
+numberofgenes = int(len(genes_all))
+
+obs_all = model.getObservableIds()
+
+title = str()
+
+for k in range(len(STIMligs)):
+    title = title + STIMligs_id[k]+'='+str(STIMligs[k]) + " "
+
+
+mpl.rcParams['figure.dpi'] = 300
+
+#%%
+
+species_CC_dash_2 = ['ppERK', 'ppAKT', 'pcFos_cJun', 'cMyc', 'Cd', 'Cdk46',
+                     'Cd_Cdk46', 'Cd_Cdk46_pRB', 'Cd_Cdk46_pRB_E2F', 'pRB',
+                     'pRBp', 'pRBpp', 'pRB_E2F','pRBp_E2F','p107_E2Frep',
+                     'p130_E2Frep', 'E2F', 'E2Frep', 'E2Fatrep', 'Cd_Cdk46_p18', 
+                     'Cd_Cdk46_p19', 'Cd_Cdk46_p21', 'Cd_Cdk46_p27',
+                     'Ce', 'Ce_Cdk2', 'Ce_Cdk2_pRBp', 'Ce_Cdk2_pRBp_E2F',
+                     'Ce_Cdk2_p21', 'Ce_Cdk2_p57', 'Ce_Cdk2_p27',
+                     'Ca', 'Ca_Cdk2', 'Ca_Cdk2_p21', 'Ca_Cdk2_p27',
+                     'p18', 'p19', 'p21', 'p27', 'p107', 'p130']                     
+
+k=0
+
+cc_dash_species, axs_s = plt.subplots(10,4, sharex='col', figsize = (6,7))
+plt.subplots_adjust(hspace = 0.6,wspace = 0.25)
+
+cc_dash_species.suptitle(title,fontsize=5,y=0.92)
+
+for i in range(10):
+    for j in range(4):
+        if k == len(species_CC_dash_2):
+            break
+        else:
+            y_val = xoutS_all[:, list(species_all).index(species_CC_dash_2[k])]
+            axs_s[i,j].plot(tout_all/3600, y_val, 'b-')            
+            axs_s[i,j].set_ylim(0,max(y_val)*1.2)
+            axs_s[i,j].tick_params(axis='both', which='major', labelsize='4')
+            axs_s[i,j].ticklabel_format(useOffset=False, style='plain')
+            axs_s[i,j].title.set_text('species: '+species_CC_dash_2[k]+' (nM)')
+            axs_s[i,j].title.set_size(5)
+            if i == 9:
+                axs_s[i,j].set_xlabel('time(h)', fontsize=5)
+            k +=1
+
+
+
+#%%
+
+obs_CC_dash = ['ERK', 'AKT', 'Fos', 'Jun', 'Myc', 'Cd', 'Cdk46', 'Cdk1', 'Cdk2', 'RB', 'E2F', 'Ce', 'Ca', 'Cb', 'Skp2', 'Pai', 'Pei', 'Pbi', 'p27', 'p107', 'p130', 'Cdc20','Cdh1a', 'Chk1', 'p21', 'p18', 'p19', 'p57', 'E2Frep','E2Fatrep']
+
+k=0
+obs_all = model.getObservableIds()
+cc_dash_obs, axs_o = plt.subplots(10,3, sharex='col', figsize = (5,7))
+
+cc_dash_obs.suptitle(title,fontsize=5,y=0.92)
+
+
+plt.subplots_adjust(hspace = 0.8, wspace=0.25)
+
+for i in range(10):
+    for j in range(3):
+        if k == len(obs_CC_dash):
+            break
+        else:
+            y_val = xoutObs_all[:, list(obs_all).index(obs_CC_dash[k])]
+            axs_o[i,j].plot(tout_all/3600, y_val, 'g-')            
+            axs_o[i,j].set_ylim(0,max(y_val)*1.2)
+            axs_o[i,j].tick_params(axis='both', which='major', labelsize='4')
+            axs_o[i,j].ticklabel_format(useOffset=False, style='plain')
+            axs_o[i,j].title.set_text('obs: '+obs_CC_dash[k]+' (nM)')
+            axs_o[i,j].title.set_size(5)
+            if i == 9:
+                axs_o[i,j].set_xlabel('time(h)', fontsize=5)
+            k +=1
+
+#%%
+
+mrna_CC = list(genes_all[[5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,25,26,27,28,29,144,145,146,147,148,149,150]])
+#mrna_CC_data = pd.read_csv('input_files/OmicsData.txt', sep='\t', index_col=0, header=0)['Exp RNA'][[14,15,16,17,19,20,21,22,23,24,25,26,27,28,29]]
+
+
+#x_m = xoutG_all[:,282:][:, list(genes_all).index(mrna_CC[2])]
+
+
+cc_dash_mrna, axs_m = plt.subplots(9,3, sharex='col', figsize = (5,7))
+plt.subplots_adjust(hspace = 0.8)
+
+cc_dash_mrna.suptitle(title,fontsize=5,y=0.92)
+
+
+k=0
+for i in range(9):
+    for j in range(3):
+        if k == len(mrna_CC):
+            break
+        else:
+            y_val = xoutG_all[:, (numberofgenes*2):][:, list(genes_all).index(mrna_CC[k])]
+            axs_m[i,j].plot(tout_all/3600, y_val,'r-')
+            #axs_m[i,j].axhline(y=mrna_CC_data[k],c='red')
+            axs_m[i,j].set_ylim(0,max(y_val)*1.2)
+            axs_m[i,j].tick_params(axis='both', which='major', labelsize='4')
+            axs_m[i,j].ticklabel_format(useOffset=False, style='plain')
+            axs_m[i,j].title.set_text('mrna: '+mrna_CC[k]+' (mpc)')
+            axs_m[i,j].title.set_size(5)
+            if i == 8:
+                axs_m[i,j].set_xlabel('time(h)', fontsize=5)
+            k +=1
 
 
 #%% Dash vTC
